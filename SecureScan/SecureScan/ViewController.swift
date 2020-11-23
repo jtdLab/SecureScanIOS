2348099cc255221a7d635782abd7f71bf814d29d
Jonas Schlauch <mrjosch99@gmail.com>
2020-11-24 00:25:00 +0100

    initial commit

---
+++ SecureScan/SecureScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecureScan
+//
+//  Created by Jonas Schlauch on 24.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
